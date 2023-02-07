a84f261882c1688797d4195c4975572b63c5b7e1
TomasVega23 <113997677+TomasVega23@users.noreply.github.com>
2023-02-07 19:14:22 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import PromoApp from "./components/PromoApp";
+import ItemListContainer from "./components/ItemListContainer";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <ItemListContainer greeting={"No se encontraron Productos!"} />
       <PromoApp />
       <Footer/>
     </div>
