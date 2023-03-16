3916a0337bf169dcb5439a1c1881e6e554201535
TomasVega23 <113997677+TomasVega23@users.noreply.github.com>
2023-03-16 11:17:40 -0300

    commit

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -7,8 +7,8 @@ const ThankYou = () => {
         <div className="container my-5">
             <div className="row">
                 <div className="col-md-12 text-center">
-                    {orderId ? <div className="alert alert-warning text-center" role="alert"><h3>Gracias por tu Compra!</h3><p>Se generó una Orden de Compra con el ID: <b>{orderId}</b></p></div> : ""}
-                    <Link to={"/"} className="btn btn-warning">Voler al Menú Principal</Link>
+                    {orderId ? <div className="alert alert-dark text-center" role="alert"><h3>Gracias por tu Compra!</h3><p>Se generó una Orden de Compra con el ID: <b>{orderId}</b></p></div> : ""}
+                    <Link to={"/"} className="btn btn-dark">Voler al Menú Principal</Link>
                 </div>
             </div>
         </div>
